23cc147e4e17d1a87c75272134d80f1c2f2962ce
Yuri Silva <yrgamerbr@gmail.com>
2023-12-18 09:01:07 -0300

    style: initial

--- src/app/(general)/page.tsx
+++ src/app/(general)/page.tsx
@@ -1,3 +1,4 @@
+import Presentation from "@/components/Presentation";
 import { Metadata } from "next";
 import Link from "next/link";
 
@@ -8,16 +9,8 @@ export const metadata: Metadata = {
 const page = () => {
   return (
     <div className="flex flex-1 flex-col items-center justify-center gap-2">
-      <p className="uppercase">
-        This website is currently under development 😆
-      </p>
-      <Link
-        className="underline underline-offset-2"
-        target="_blank"
-        href="https://www.linkedin.com/in/yuirsilva/"
-      >
-        LINKEDIN
-      </Link>
+      <Presentation />
+      <p>this is the new black xd</p>
     </div>
   );
 };
