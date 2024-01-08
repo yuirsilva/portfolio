f59b00ed5b54ed7c4ff22601c3b24e36e04b31b0
Yuri Silva <yrgamerbr@gmail.com>
2024-01-08 19:55:50 -0300

    style: more animations, more animations, more animations

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,5 @@
 import MagneticButton from "@/components/MagneticButton";
+import Roll from "@/components/Roll";
 import { FC } from "react";
 
 interface FooterProps {}
@@ -6,7 +7,14 @@ interface FooterProps {}
 const Footer: FC<FooterProps> = ({}) => {
   return (
     <footer className="flex justify-between px-12 pb-8 max-[320px]:justify-end md:px-16">
-      <p className="max-[320px]:hidden">yuri silva</p>
+      {/* <p className="max-[320px]:hidden">yuri silva</p> */}
+      <div className="group select-none">
+        <Roll
+          initial="top-full"
+          className="max-[320px]:hidden"
+          text="yuri silva"
+        />
+      </div>
       <div className="flex gap-6 uppercase">
         <MagneticButton>
           <a href="https://CHANGE_THIS">LinkedIn</a>
