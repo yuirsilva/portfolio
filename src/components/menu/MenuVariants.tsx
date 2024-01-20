51703a4975990a8a4f4524a3b73b43ab2f3cf8ed
Yuri Silva <yrgamerbr@gmail.com>
2024-01-20 08:17:02 -0300

    style: few menu changes

--- src/components/menu/MenuVariants.tsx
+++ src/components/menu/MenuVariants.tsx
@@ -2,35 +2,26 @@ import { SECONDARY_TRANSITION } from "@/lib/utils";
 import { Variants } from "framer-motion";
 
 export const menuVariants: Variants = {
-  open: {
-    y: "0%",
-    transition: SECONDARY_TRANSITION,
-  },
-  closed: {
-    y: "-100%",
-    transition: SECONDARY_TRANSITION,
-  },
+  open: { y: "0%", transition: SECONDARY_TRANSITION },
+  closed: { y: "-100%", transition: SECONDARY_TRANSITION },
 };
 
 export const menuBackgroundVariants: Variants = {
   open: {
-    ...menuVariants.open,
     backgroundColor: "rgb(38 38 38 / 0.2)",
+    userSelect: "initial",
+    pointerEvents: "initial",
+    transition: SECONDARY_TRANSITION,
   },
   closed: {
-    ...menuVariants.closed,
     backgroundColor: "rgb(38 38 38 / 0)",
+    pointerEvents: "none",
+    userSelect: "none",
+    transition: SECONDARY_TRANSITION,
   },
 };
 
 export const menuMainVariants: Variants = {
-  open: {
-    ...menuVariants.open,
-    // boxShadow:
-    //   "0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1)",
-  },
-  closed: {
-    ...menuVariants.closed,
-    // boxShadow: "0 10px 15px -3px rgb(0 0 0 / 0), 0 4px 6px -4px rgb(0 0 0 / 0)",
-  },
+  open: menuVariants.open,
+  closed: menuVariants.closed,
 };
