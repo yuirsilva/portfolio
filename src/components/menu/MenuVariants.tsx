33d4eb39276a753b91140748c70437bdb18a4a79
Yuri Silva <yrgamerbr@gmail.com>
2024-01-20 07:51:21 -0300

    fixed: test

--- src/components/menu/MenuVariants.tsx
+++ src/components/menu/MenuVariants.tsx
@@ -4,14 +4,14 @@ import { Variants } from "framer-motion";
 export const menuBackgroundVariants: Variants = {
   open: {
     backgroundColor: "rgb(38 38 38 / 0.2)",
-    userSelect: "initial",
-    pointerEvents: "initial",
+    // userSelect: "initial",
+    // pointerEvents: "initial",
     transition: SECONDARY_TRANSITION,
   },
   closed: {
     backgroundColor: "rgb(38 38 38 / 0)",
-    pointerEvents: "none",
-    userSelect: "none",
+    // pointerEvents: "none",
+    // userSelect: "none",
     transition: SECONDARY_TRANSITION,
   },
 };
@@ -30,11 +30,11 @@ export const menuVariants: Variants = {
 export const menuMainVariants: Variants = {
   open: {
     ...menuVariants.open,
-    boxShadow:
-      "0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1)",
+    // boxShadow:
+    //   "0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1)",
   },
   closed: {
     ...menuVariants.closed,
-    boxShadow: "0 10px 15px -3px rgb(0 0 0 / 0), 0 4px 6px -4px rgb(0 0 0 / 0)",
+    // boxShadow: "0 10px 15px -3px rgb(0 0 0 / 0), 0 4px 6px -4px rgb(0 0 0 / 0)",
   },
 };
