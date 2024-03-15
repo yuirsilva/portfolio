a3f8ab1d7b6a1ee65e22f9fa6e64b343d8bfe34a
Yuri Silva <yrgamerbr@gmail.com>
2024-03-15 15:47:49 -0300

    feat: blog!

---
+++ src/content/quotes.ts
@@ -0,0 +1,19 @@
+export const four_oh_four = new Array<string>(
+    "A sad trombone sound effect.",
+    "A sense of disappointment as you realize that you won't be able to access the information you were looking for.",
+    "The sound of a 404 error message dinging.",
+    "The feeling of frustration and disappointment.",
+    "A pile of clothes on the floor with a missing sock.",
+    "The feeling of disappointment when you realize that you're lost."
+);
+
+export const posts_index = new Array<string>(
+    "The feeling of accomplishment after completing a difficult task.",
+    "A warm hug from a loved one.",
+    "The smell of freshly baked bread.",
+    "A sense of peace when you are surrounded by people who love you.",
+    "A cool breeze blowing through the trees.",
+    "A clear blue sky with fluffy white clouds.",
+    "A calming, muted color palette.",
+    "You close your eyes and take a deep breath, feeling the stress of the day melt away."
+);
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -69,7 +69,7 @@ const config = {
             },
         },
     },
-    plugins: [],
+    plugins: [require("@tailwindcss/typography")],
 } satisfies Config;
 
 export default config;
